47d475c80a1abfaaffb8d9880c8d9deb554254ce
simran9524 <simrankalra9524@gmail.com>
2019-02-23 14:18:24 -0500

    addind crud

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -5,5 +5,12 @@
         "font-size: 20px; color: blue; font-weight: bold;");
     }
 
+    $(".btn-danger").click(function(event){
+        if(!confirm("Are you sure?")){
+            event.preventDefault();
+            window.location.assign("/contact-list");
+        }
+    });
+
     window.addEventListener("load", Start);
 })();
--- routes/contact.js
+++ routes/contact.js
@@ -3,11 +3,11 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 // create a reference to the contact model
-let contact = require('../models/contact');
+let contactModel = require('../models/contact');
 
 /* GET contact list page - READ */
 router.get('/', (req, res, next) => {
-    contact.find((err, contactList) => {
+    contactModel.find((err, contactList) => {
         
         if(err) {
             return console.error(err);
@@ -24,5 +24,77 @@ router.get('/', (req, res, next) => {
     })
 });
 
+router.get('/add', (req, res, next)=>{
+    res.render('contacts/add', {
+        title: 'Add your Contact'
+    });
+});
+
+router.post('/add', (req, res, next)=>{
+    
+    //console.log(req.body);
+    let newContact = contactModel({
+        "firstName": req.body.firstName,            
+        "lastName": req.body.lastName,
+        "age": req.body.age            
+    });
+
+    contactModel.create(newContact, (err, contactModel) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }else{
+            res.redirect('/contact-list');
+        }
+    });
+});
+
+router.get('/edit/:id', (req, res, next)=>{
+    let id= req.params.id;
+    contactModel.findById(id, (err, contactObject)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }else{
+            res.render('contacts/edit', {
+                title: 'Edit contact',
+                contact: contactObject
+            });
+        }
+    });
+});
+
+router.post('/edit/:id', (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedContact = contactModel({
+        "_id": id,
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.update({_id: id}, updatedContact, (err)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }else{
+            res.redirect('/contact-list');
+        }
+    });
+});
+
+router.get('/delete/:id', (req, res, next) => {
+    let id=req.params.id;
+
+    contactModel.remove({_id: id}, (err) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('/contact-list');
+        }
+    });
+});
 
 module.exports = router;
